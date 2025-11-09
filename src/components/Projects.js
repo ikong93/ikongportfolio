359616b0d0e9870b544cb0845fbdc64d7ba165a1
ikong93 <rodnietg@gmail.com>
2025-11-09 13:57:31 +0800

    links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,8 +5,8 @@ function Projects() {
     <section>
       <h2>Projects</h2>
       <ul>
-        <a href="https://wondrous-marshmallow-59b80e.netlify.app/"><li>Personal Portfolio Website</li></a>
-        <a href="https://wondrous-marshmallow-59b80e.netlify.app/"><li>To-Do App</li></a>
+        <a href="https://ikongportfolio.netlify.app/"><li>Personal Portfolio Website</li></a>
+        <a href="https://taskflowikong.netlify.app/"><li>To-Do App</li></a>
         <li>Blog App</li>
       </ul>
     </section>
