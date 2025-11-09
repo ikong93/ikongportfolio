ab283b6a5afd483c3e83ecf08f977caa0ad3d4ec
ikong93 <rodnietg@gmail.com>
2025-11-09 13:17:53 +0800

    portfolio link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,7 @@ function Projects() {
     <section style={{ padding: "2rem" }}>
       <h2>Projects</h2>
       <ul>
-        <li>Personal Portfolio Website</li>
+        <li> <a href="https://wondrous-marshmallow-59b80e.netlify.app/">Personal Portfolio Website</a></li>
         <li>To-Do App</li>
         <li>Blog App</li>
       </ul>
